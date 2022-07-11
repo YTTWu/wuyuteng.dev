029b3a86f0e9de100a79ea9705170051d229c39b
Yuteng Terrence Wu <yutengwu@yahoo.com>
2022-07-10 22:43:08 -0700

    Updates

---
+++ precache-manifest.b26ff9a28b49ad7a992ec2683dee76d5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d798e6735448da8b58f",
+    "url": "/wuyuteng.dev/static/css/main.2e2f22f1.chunk.css"
+  },
+  {
+    "revision": "2d798e6735448da8b58f",
+    "url": "/wuyuteng.dev/static/js/main.6a14e480.chunk.js"
+  },
+  {
+    "revision": "39e3f011aadd0736410f",
+    "url": "/wuyuteng.dev/static/js/runtime~main.cb0e1c84.js"
+  },
+  {
+    "revision": "15f5565b138703c8fc27",
+    "url": "/wuyuteng.dev/static/js/2.ed7858ef.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/wuyuteng.dev/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "6e5a79cf9308cf7d66095e561369e4d0",
+    "url": "/wuyuteng.dev/static/media/id_photo.6e5a79cf.jpg"
+  },
+  {
+    "revision": "dfd251fc812a1e76bc089f3bc2749a18",
+    "url": "/wuyuteng.dev/static/media/bear.dfd251fc.jpg"
+  },
+  {
+    "revision": "ca58ccf1ed880c6f1bb6df53cbea8ab0",
+    "url": "/wuyuteng.dev/static/media/DONOR_image.ca58ccf1.bin"
+  },
+  {
+    "revision": "297ad46ce71f7fa3dd75a78f17b13917",
+    "url": "/wuyuteng.dev/static/media/signature.297ad46c.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/wuyuteng.dev/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "f5391be6a3a19ccc2163288b25046dd7",
+    "url": "/wuyuteng.dev/index.html"
+  }
+];
